24d6c5226d1151e1bf7ec5de29270eee468cf35a
plsgard <pier-lionel.sgard@goood.pro>
2016-01-24 00:02:26 +0100

    JIR-7 #time 10m création des constructeurs du client rest

--- Sources/Jira.NET.Tests/Properties/AssemblyInfo.cs
+++ Sources/Jira.NET.Tests/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Jira.NET")]
-[assembly: AssemblyDescription("Jira.NET wrapper for Jira Software using Jira Software REST API")]
+[assembly: AssemblyTitle("Jira.NET.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Goood!")]
-[assembly: AssemblyProduct("Jira.NET")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jira.NET.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("Software")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("b88e0d05-02fb-4be2-b706-a8bf82b67401")]
+[assembly: Guid("8facb146-efb0-4c48-8744-766915571787")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les numéros de build et de révision 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Sources/Jira.NET/JiraRestClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.NET
+{
+    public class JiraRestClient
+    {
+        private string ApiUrl = "jira/rest/api/2/";
+        public Uri ServerUrl { get; }
+        public Uri BaseUrl => new Uri(ServerUrl, ApiUrl);
+
+        public JiraRestClient(string serverUrl)
+        {
+            if (string.IsNullOrWhiteSpace(serverUrl))
+                throw new ArgumentNullException(nameof(serverUrl));
+            Uri result;
+            if (!Uri.TryCreate(serverUrl, UriKind.Absolute, out result))
+                throw new FormatException("The server url provided is not a valid URI.");
+            ServerUrl = result;
+        }
+
+        public JiraRestClient(Uri serverUrl)
+        {
+            if (serverUrl == null)
+                throw new ArgumentNullException(nameof(serverUrl));
+
+            ServerUrl = serverUrl;
+        }
+    }
+}
