73b8b1f537bf946ae1602d0acc4372fa5a159758
plsgard <pier-lionel.sgard@goood.pro>
2016-04-27 18:50:02 +0200

    JIR-7 #time 5m gestion des identifiants

--- Sources/Jira.NET.Tests/JiraBasicRestClientTests.cs
+++ Sources/Jira.NET.Tests/JiraBasicRestClientTests.cs
@@ -1,4 +1,5 @@
-﻿using Jira.NET.Models;
+﻿using System.Configuration;
+using Jira.NET.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Jira.NET.Tests
@@ -6,21 +7,24 @@ namespace Jira.NET.Tests
     [TestClass]
     public class JiraBasicRestClientTests
     {
+        private static readonly string JiraTestUrl = ConfigurationManager.AppSettings["jira:test:url"];
+        private static readonly string JiraTestUser = ConfigurationManager.AppSettings["jira:test:user"];
+        private static readonly string JiraTestPassword = ConfigurationManager.AppSettings["jira:test:password"];
+
         [TestMethod]
         public void JiraBasicRestClient_Ctor_IsAuthenticated()
         {
-            JiraBasicRestClient client = new JiraBasicRestClient("http://projects.goood.pro:8080", "dev", "P@ssw0rd");
+            JiraBasicRestClient client = new JiraBasicRestClient(JiraTestUrl, JiraTestUser, JiraTestPassword);
             Assert.IsTrue(client.IsAuthenticated);
         }
 
         [TestMethod]
         public void JiraBasicRestClient_GetCurrentUser()
         {
-            const string login = "dev";
-            JiraBasicRestClient client = new JiraBasicRestClient("http://projects.goood.pro:8080", login, "P@ssw0rd");
+            JiraBasicRestClient client = new JiraBasicRestClient(JiraTestUrl, JiraTestUser, JiraTestPassword);
             JiraUser currentUser = client.GetCurrentUser();
             Assert.IsNotNull(currentUser);
-            Assert.AreEqual(login, currentUser.Name);
+            Assert.AreEqual(JiraTestUser, currentUser.Name);
             Assert.IsNotNull(currentUser.AvatarUrls);
             Assert.IsNotNull(currentUser.Groups);
             Assert.IsNotNull(currentUser.ApplicationRoles);
